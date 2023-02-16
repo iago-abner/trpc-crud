f473840425dbf5d3b4aad6a1da8875684874aaf8
Iago Abner <iagoabner.dev@gmail.com>
2023-02-16 16:30:16 -0300

    feat: set note route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
+import * as trpcExpress from '@trpc/server/adapters/express'
 import express from 'express'
+import { notesRouter } from './routes/notes'
+import { context, router } from './trpc'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return { message: 'Home' }
+const appRouter = router({
+  note: notesRouter
 })
+
+app.use('/trpc', trpcExpress.createExpressMiddleware({
+  router: appRouter,
+  createContext: context
+}))
 
 export default app
